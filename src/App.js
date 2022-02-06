ff923435838b556cb4bef333288a965f6d771e42
simenghe <simengschool@gmail.com>
2022-02-06 17:49:52 -0500

    Frontend moment

--- src/App.js
+++ src/App.js
@@ -2,26 +2,51 @@
 Grid of credit cards that you can and add or delete.
 Click on a specific card to get more information.
 Rating system on each card.
-*/
-import logo from './logo.svg';
-import './App.css';
 
+Create a form for creating cards
+{
+  name: "Bob",
+  email: "bob@gmail.com",
+  number: "12344213123",
+}
+*/
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+import { v4 as uuid } from "uuid";
+import { useState } from "react";
+
+const sampleCard = {
+  id: uuid(),
+  username: "Bob Username",
+  email: "bob@gmail.com",
+};
 function App() {
+  const [cards, setCards] = useState(new Array(4).fill(sampleCard));
+  console.log(cards);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="w-full bg-green-900">
+          <label className="block">
+            <span className="block text-sm font-medium text-white">
+              Username
+            </span>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="email"
+              placeholder="Username"
+            ></input>
+          </label>
+        </form>
+
+        <div className="grid grid-rows-4 grid-cols-2 gap-4 p-6 w-screen">
+          {cards &&
+            cards.map((x, i) => {
+              return <Card key={i} username={x.username} />;
+            })}
+        </div>
       </header>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,18 @@
-export default function Card() {
-  return <div></div>;
+import { useState } from "react";
+
+export default function Card({ username }) {
+  const [blur, setBlur] = useState(true);
+  return (
+    <div
+      className={`text-black h-full rounded-lg border drop-shadow-lg bg-green-300 ${
+        blur ? "blur-sm" : "blur-none"
+      } hover:bg-green-900 aspect-video`}
+      onClick={() => setBlur((prev) => !prev)}
+    >
+      <h1>{username}</h1>
+      <h1>{username}</h1>
+      <h1>{username}</h1>
+      <h1>{username}</h1>
+    </div>
+  );
 }
