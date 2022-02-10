7b2c49bb800a3b84d1a6ea7eadb271bcbea595a0
simenghe <simengschool@gmail.com>
2022-02-10 14:55:23 -0500

    cool

--- src/App.js
+++ src/App.js
@@ -22,34 +22,61 @@ const sampleCard = {
   username: "Bob Username",
   email: "bob@gmail.com",
 };
+
+const initialForm = {
+  username: "",
+};
+
 function App() {
   const [curCards, setCurCards] = useState(new Array(4).fill(sampleCard));
   const [cards, setCards] = useContext(CardContext);
+  const [curForm, setCurForm] = useState(initialForm);
+
   console.log(curCards);
   console.log(cards);
+
   useEffect(() => {
     // setCards({ ...cards, caner: "trance" });
     setCards((prev) => {
       let newState = { ...prev, wezer: "madem" };
       return newState;
     });
-  }, []);
+  }, [setCards]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <form className="w-full bg-green-900">
-          <label className="block">
-            <span className="block text-sm font-medium text-white">
-              Username {JSON.stringify(cards)}
-            </span>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="email"
-              placeholder="Username"
-            ></input>
-          </label>
-        </form>
+        <div className="p-10">
+          <form
+            className="w-full bg-green-900"
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log("Submitted, ", curForm);
+            }}
+          >
+            <label className="block">
+              <span className="block text-sm font-medium text-white">
+                Username {JSON.stringify(cards)}
+              </span>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="email"
+                onChange={(e) => {
+                  setCurForm((prev) => ({
+                    ...prev,
+                    [e.target.id]: e.target.value,
+                  }));
+                  console.log("CURFORM", curForm);
+                }}
+                placeholder="Username"
+              ></input>
+            </label>
+            <button className="py-10" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
 
         <div className="grid grid-rows-4 grid-cols-2 gap-4 p-6 w-screen">
           {curCards &&
